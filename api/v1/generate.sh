095d9538b90840c268183ced605ad4f6c58dadfa
Brady Love <blove@pivotal.io>
2017-02-08 14:10:41 -0700

    Add V1 API definition for adapter
    [#137746723]

---
+++ api/v1/generate.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+dir_resolve()
+{
+    cd "$1" 2>/dev/null || return $?  # cd to desired directory; if fail, quell any error messages but return exit status
+    echo "`pwd -P`" # output full, link-resolved path
+}
+
+set -e
+
+TARGET=`dirname $0`
+TARGET=`dir_resolve $TARGET`
+cd $TARGET
+
+go get github.com/golang/protobuf/{proto,protoc-gen-go}
+
+
+tmp_dir=$(mktemp -d)
+mkdir -p $tmp_dir/scalable-syslog
+
+cp $GOPATH/src/github.com/cloudfoundry-incubator/scalable-syslog/api/v1/*proto $tmp_dir/scalable-syslog
+cp *.proto $tmp_dir/scalable-syslog
+
+protoc $tmp_dir/scalable-syslog/*.proto --go_out=plugins=grpc:. --proto_path=$tmp_dir/scalable-syslog
+
+rm -r $tmp_dir
