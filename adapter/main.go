a0e37a523dbc626ee64f99bfac035c4cec60f8b4
Eno Compton <ecompton@pivotal.io>
2017-02-15 13:34:11 -0700

    Inline flags in adapter main
    [#137747845]

--- adapter/main.go
+++ adapter/main.go
@@ -11,19 +11,17 @@ import (
 	"github.com/cloudfoundry-incubator/scalable-syslog/api"
 )
 
-var (
-	healthHostport  = flag.String("health", ":8080", "The hostport to listen for health requests")
-	adapterHostport = flag.String("addr", ":4443", "The hostport to for the adapter controller")
-	pprofHostport   = flag.String("pprof", "localhost:6060", "The hostport to listen for pprof")
-
-	caFile     = flag.String("ca", "", "The file path for the CA cert")
-	certFile   = flag.String("cert", "", "The file path for the adapter server cert")
-	keyFile    = flag.String("key", "", "The file path for the adapter server key")
-	commonName = flag.String("cn", "", "The common name used for the TLS config")
-)
-
 func main() {
+	healthHostport  := flag.String("health", ":8080", "The hostport to listen for health requests")
+	adapterHostport := flag.String("addr", ":4443", "The hostport to for the adapter controller")
+	pprofHostport   := flag.String("pprof", "localhost:6060", "The hostport to listen for pprof")
+
+	caFile     := flag.String("ca", "", "The file path for the CA cert")
+	certFile   := flag.String("cert", "", "The file path for the adapter server cert")
+	keyFile    := flag.String("key", "", "The file path for the adapter server key")
+	commonName := flag.String("cn", "", "The common name used for the TLS config")
 	flag.Parse()
+
 	log.Print("Starting adapter...")
 	defer log.Print("Closing adapter.")
 
@@ -32,13 +30,13 @@ func main() {
 		log.Fatalf("Invalid TLS config: %s", err)
 	}
 
-	healthHostport, controllerHealthport := app.StartAdapter(
+	healthAddr, controllerAddr := app.StartAdapter(
 		app.WithHealthAddr(*healthHostport),
 		app.WithControllerAddr(*adapterHostport),
 		app.WithControllerTLSConfig(tlsConfig),
 	)
-	log.Printf("Health endpoint is listening on %s", healthHostport)
-	log.Printf("Adapter controller is listening on %s", controllerHealthport)
+	log.Printf("Health endpoint is listening on %s", healthAddr)
+	log.Printf("Adapter controller is listening on %s", controllerAddr)
 
 	log.Println(http.ListenAndServe(*pprofHostport, nil))
 }
