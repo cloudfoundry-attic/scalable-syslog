d7887a6a9a82b0ec504666ad0108afaf0fedebcd
Eno Compton <ecompton@pivotal.io>
2017-04-19 16:23:49 -0600

    Clean up scheduler tests
    [#143841877]
    
    Signed-off-by: Jason Keene <jkeene@pivotal.io>

--- scheduler/app/scheduler_test.go
+++ scheduler/app/scheduler_test.go
@@ -1,6 +1,7 @@
 package app_test
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -20,304 +21,208 @@ import (
 	. "github.com/onsi/gomega"
 )
 
-var _ = Describe("Scheduler - End to End", func() {
-	var (
-		scheduler         *app.Scheduler
-		dataSource        *httptest.Server
-		testAdapterServer *testAdapterServer
-		bindings          []*v1.Binding
-		healthAddr        string
-		opts              []app.SchedulerOption
-	)
+var _ = Describe("Scheduler", func() {
+	It("reports health info", func() {
+		dataSource := httptest.NewServer(&fakeCC{
+			results: results{
+				"9be15160-4845-4f05-b089-40e827ba61f1": appBindings{
+					Hostname: "org.space.name",
+					Drains:   []string{"syslog://1.1.1.1/"},
+				},
+			},
+		})
+		healthAddr, _ := startScheduler(dataSource.URL, defaultOps())
 
-	BeforeEach(func() {
+		f := func() []byte {
+			resp, err := http.Get(fmt.Sprintf("http://%s/health", healthAddr))
+			Expect(err).ToNot(HaveOccurred())
+			Expect(resp.StatusCode).To(Equal(http.StatusOK))
+
+			body, err := ioutil.ReadAll(resp.Body)
+			Expect(err).ToNot(HaveOccurred())
+			return body
+		}
+		Eventually(f, 3*time.Second, 500*time.Millisecond).Should(MatchJSON(`
+				{
+					"drainCount": 1,
+					"adapterCount": 1,
+					"blacklistedOrInvalidUrlCount": 0
+				}
+			`))
+	})
+
+	It("ignores blacklisted syslog URLs", func() {
+		dataSource := httptest.NewServer(&fakeCC{
+			results: results{
+				"9be15160-4845-4f05-b089-40e827ba61f1": appBindings{
+					Hostname: "org.space.name",
+					Drains: []string{
+						"syslog://1.1.1.1/",
+						"syslog://14.15.16.18/", // blacklisted
+					},
+				},
+			},
+		})
 		blacklistIPs, err := ingress.NewIPRanges(
-			ingress.IPRange{Start: "14.15.16.17", End: "14.15.16.20"},
+			ingress.IPRange{
+				Start: "14.15.16.17",
+				End:   "14.15.16.20",
+			},
 		)
 		Expect(err).ToNot(HaveOccurred())
-		opts = []app.SchedulerOption{
-			app.WithHealthAddr("localhost:0"),
-			app.WithPollingInterval(time.Millisecond),
-			app.WithBlacklist(blacklistIPs),
-			app.WithOptIn(true),
-		}
-		bindings = []*v1.Binding{
+		opts := defaultOps()
+		opts = append(opts, app.WithBlacklist(blacklistIPs))
+		_, testAdapterServer := startScheduler(dataSource.URL, opts)
+
+		expectedRequests := []*v1.CreateBindingRequest{
 			{
-				AppId:    "9be15160-4845-4f05-b089-40e827ba61f1",
-				Drain:    "syslog://14.15.16.21/?drain-version=2.0",
-				Hostname: "org.space.logspinner",
-			},
-			{
-				AppId:    "9be15160-4845-4f05-b089-40e827ba61f1",
-				Drain:    "syslog://14.15.16.16/?drain-version=2.0",
-				Hostname: "org.space.logspinner",
+				Binding: &v1.Binding{
+					AppId:    "9be15160-4845-4f05-b089-40e827ba61f1",
+					Drain:    "syslog://1.1.1.1/",
+					Hostname: "org.space.name",
+				},
 			},
 		}
-	})
-
-	JustBeforeEach(func() {
-		lis, err := net.Listen("tcp", "localhost:0")
-		Expect(err).ToNot(HaveOccurred())
-
-		adapterTLSConfig, err := api.NewMutualTLSConfig(
-			Cert("adapter.crt"),
-			Cert("adapter.key"),
-			Cert("scalable-syslog-ca.crt"),
-			"adapter",
-		)
-		if err != nil {
-			log.Fatalf("Invalid TLS config: %s", err)
+		lenCheck := func() int {
+			return len(testAdapterServer.ActualCreateBindingRequest)
 		}
-
-		testAdapterServer = NewTestAdapterServer()
-		grpcServer := grpc.NewServer(
-			grpc.Creds(credentials.NewTLS(adapterTLSConfig)),
-		)
-		v1.RegisterAdapterServer(grpcServer, testAdapterServer)
-
-		go grpcServer.Serve(lis)
-
-		tlsConfig, err := api.NewMutualTLSConfig(
-			Cert("scheduler.crt"),
-			Cert("scheduler.key"),
-			Cert("scalable-syslog-ca.crt"),
-			"adapter",
-		)
-		Expect(err).ToNot(HaveOccurred())
-
-		scheduler = app.NewScheduler(
-			dataSource.URL,
-			[]string{lis.Addr().String()},
-			tlsConfig,
-			opts...,
-		)
-		healthAddr = scheduler.Start()
+		Eventually(lenCheck).Should(Equal(1))
+		var actualRequests []*v1.CreateBindingRequest
+		f := func() []*v1.CreateBindingRequest {
+			select {
+			case req := <-testAdapterServer.ActualCreateBindingRequest:
+				actualRequests = append(actualRequests, req)
+			default:
+			}
+			return actualRequests
+		}
+		Eventually(f).Should(ConsistOf(expectedRequests))
 	})
 
-	Context("when CC continuously returns data", func() {
-		BeforeEach(func() {
-			dataSource = httptest.NewServer(&fakeCC{})
+	It("services only URLs with drain-version=2.0 when opt-in is enabled", func() {
+		dataSource := httptest.NewServer(&fakeCC{
+			results: results{
+				"9be15160-4845-4f05-b089-40e827ba61f1": appBindings{
+					Hostname: "org.space.name",
+					Drains: []string{
+						"syslog://1.1.1.1/?drain-version=2.0",
+						"syslog://2.2.2.2/",
+					},
+				},
+			},
 		})
-
-		It("reports health info", func() {
-			f := func() []byte {
-				resp, err := http.Get(fmt.Sprintf("http://%s/health", healthAddr))
-				Expect(err).ToNot(HaveOccurred())
-				Expect(resp.StatusCode).To(Equal(http.StatusOK))
-
-				body, err := ioutil.ReadAll(resp.Body)
-				Expect(err).ToNot(HaveOccurred())
-				return body
-			}
-			Eventually(f, 3*time.Second, 500*time.Millisecond).Should(MatchJSON(`
+		opts := append(defaultOps(), app.WithOptIn(true))
+		_, testAdapterServer := startScheduler(dataSource.URL, opts)
+		expectedRequests := []*v1.CreateBindingRequest{
 			{
-				"drainCount": 2,
-				"adapterCount": 1,
-				"blacklistedOrInvalidUrlCount": 2
-			}`))
-		})
-
-		It("writes non-blacklisted and drain-version=2.0 bindings to the adapter", func() {
-			expectedRequests := []*v1.CreateBindingRequest{
-				{
-					Binding: bindings[0],
+				Binding: &v1.Binding{
+					AppId:    "9be15160-4845-4f05-b089-40e827ba61f1",
+					Drain:    "syslog://1.1.1.1/?drain-version=2.0",
+					Hostname: "org.space.name",
 				},
-				{
-					Binding: bindings[1],
-				},
+			},
+		}
+		lenCheck := func() int {
+			return len(testAdapterServer.ActualCreateBindingRequest)
+		}
+		Eventually(lenCheck).Should(Equal(1))
+		var actualRequests []*v1.CreateBindingRequest
+		f := func() []*v1.CreateBindingRequest {
+			select {
+			case req := <-testAdapterServer.ActualCreateBindingRequest:
+				actualRequests = append(actualRequests, req)
+			default:
 			}
-			// TODO: when we implement diffing in the scheduler this will need
-			// to change to HaveLen(2)
-			lenCheck := func() int {
-				return len(testAdapterServer.ActualCreateBindingRequest)
-			}
-			Eventually(lenCheck).Should(BeNumerically(">=", 2))
-			var actualRequests []*v1.CreateBindingRequest
-			f := func() []*v1.CreateBindingRequest {
-				select {
-				case req := <-testAdapterServer.ActualCreateBindingRequest:
-					actualRequests = append(actualRequests, req)
-				default:
-				}
-				return actualRequests
-			}
-			Eventually(f).Should(ConsistOf(expectedRequests))
-		})
+			return actualRequests
+		}
+		Eventually(f).Should(ConsistOf(expectedRequests))
 	})
 
-	Context("with opt in disabled", func() {
-		BeforeEach(func() {
-			dataSource = httptest.NewServer(&fakeCC{})
-			opts = append(opts, app.WithOptIn(false))
-			bindings = []*v1.Binding{
-				{
-					AppId:    "9be15160-4845-4f05-b089-40e827ba61f1",
-					Drain:    "syslog://14.15.16.21/",
-					Hostname: "org.space.logspinner",
+	It("tells the adapters to delete bindings", func() {
+		dataSource := httptest.NewServer(&fakeCC{
+			withEmptyResult: true,
+			results: results{
+				"9be15160-4845-4f05-b089-40e827ba61f1": appBindings{
+					Hostname: "org.space.name",
+					Drains: []string{
+						"syslog://1.1.1.1/",
+						"syslog://2.2.2.2/",
+					},
 				},
-				{
-					AppId:    "9be15160-4845-4f05-b089-40e827ba61f1",
-					Drain:    "syslog://14.15.16.21/?drain-version=2.0",
-					Hostname: "org.space.logspinner",
-				},
-				{
-					AppId:    "9be15160-4845-4f05-b089-40e827ba61f1",
-					Drain:    "syslog://14.15.16.16/?drain-version=2.0",
-					Hostname: "org.space.logspinner",
-				},
-				{
-					AppId:    "9be15160-4845-4f05-b089-40e827ba61f1",
-					Drain:    "syslog://localhost",
-					Hostname: "org.space.logspinner",
-				},
-				{
-					AppId:    "ed150c22-f866-11e6-bc64-92361f002671",
-					Drain:    "syslog://localhost",
-					Hostname: "org.space.logspinner",
-				},
-			}
+			},
 		})
-
-		It("gets logs for all bindings", func() {
-			expectedRequests := []*v1.CreateBindingRequest{
-				{
-					Binding: bindings[0],
+		_, testAdapterServer := startScheduler(dataSource.URL, defaultOps())
+		expectedRequests := []*v1.DeleteBindingRequest{
+			{
+				Binding: &v1.Binding{
+					AppId:    "9be15160-4845-4f05-b089-40e827ba61f1",
+					Drain:    "syslog://1.1.1.1/",
+					Hostname: "org.space.name",
 				},
-				{
-					Binding: bindings[1],
+			},
+			{
+				Binding: &v1.Binding{
+					AppId:    "9be15160-4845-4f05-b089-40e827ba61f1",
+					Drain:    "syslog://2.2.2.2/",
+					Hostname: "org.space.name",
 				},
-				{
-					Binding: bindings[2],
-				},
-				{
-					Binding: bindings[3],
-				},
-				{
-					Binding: bindings[4],
-				},
+			},
+		}
+		Eventually(testAdapterServer.ActualDeleteBindingRequest).Should(HaveLen(2))
+		var actualRequests []*v1.DeleteBindingRequest
+		f := func() []*v1.DeleteBindingRequest {
+			select {
+			case req := <-testAdapterServer.ActualDeleteBindingRequest:
+				actualRequests = append(actualRequests, req)
+			default:
 			}
-			// TODO: when we implement diffing in the scheduler this will need
-			// to change to HaveLen(2)
-			lenCheck := func() int {
-				return len(testAdapterServer.ActualCreateBindingRequest)
-			}
-			Eventually(lenCheck).Should(BeNumerically(">=", 2))
-			var actualRequests []*v1.CreateBindingRequest
-			f := func() []*v1.CreateBindingRequest {
-				select {
-				case req := <-testAdapterServer.ActualCreateBindingRequest:
-					actualRequests = append(actualRequests, req)
-				default:
-				}
-				return actualRequests
-			}
-			Eventually(f).Should(ConsistOf(expectedRequests))
-		})
+			return actualRequests
+		}
+		Eventually(f).Should(ConsistOf(expectedRequests))
 	})
 
-	Context("with a CC that starts returning an empty result", func() {
-		BeforeEach(func() {
-			dataSource = httptest.NewServer(&fakeCC{
-				withEmptyResult: true,
-			})
+	It("removes old bindings and create new bindings when an app is renamed", func() {
+		dataSource := httptest.NewServer(&fakeCC{
+			withRenamedApps: true,
 		})
+		_, testAdapterServer := startScheduler(dataSource.URL, defaultOps())
 
-		It("tells the adapters to delete the removed binding", func() {
-			expectedRequests := []*v1.DeleteBindingRequest{
-				{
-					Binding: bindings[0],
-				},
-				{
-					Binding: bindings[1],
-				},
-			}
-			Eventually(testAdapterServer.ActualDeleteBindingRequest).Should(HaveLen(2))
-			var actualRequests []*v1.DeleteBindingRequest
-			f := func() []*v1.DeleteBindingRequest {
-				select {
-				case req := <-testAdapterServer.ActualDeleteBindingRequest:
-					actualRequests = append(actualRequests, req)
-				default:
-				}
-				return actualRequests
-			}
-			Eventually(f).Should(ConsistOf(expectedRequests))
-		})
-	})
+		createReq := <-testAdapterServer.ActualCreateBindingRequest
+		Expect(createReq.Binding).To(Equal(&v1.Binding{
+			AppId:    "9be15160-4845-4f05-b089-40e827ba61f1",
+			Hostname: "org.space.original",
+			Drain:    "syslog://14.15.16.22/",
+		}))
 
-	Context("when an app is renamed", func() {
-		BeforeEach(func() {
-			dataSource = httptest.NewServer(&fakeCCWithRenamedApps{})
-		})
+		deleteReq := <-testAdapterServer.ActualDeleteBindingRequest
+		Expect(deleteReq.Binding).To(Equal(&v1.Binding{
+			AppId:    "9be15160-4845-4f05-b089-40e827ba61f1",
+			Hostname: "org.space.original",
+			Drain:    "syslog://14.15.16.22/",
+		}))
 
-		It("creates new adapter bindings and removes the previous bindings", func(done Done) {
-			defer close(done)
-			createReq := <-testAdapterServer.ActualCreateBindingRequest
-			Expect(createReq.Binding).To(Equal(&v1.Binding{
-				AppId:    "9be15160-4845-4f05-b089-40e827ba61f1",
-				Hostname: "org.space.original",
-				Drain:    "syslog://14.15.16.22/?drain-version=2.0",
-			}))
-
-			deleteReq := <-testAdapterServer.ActualDeleteBindingRequest
-			Expect(deleteReq.Binding).To(Equal(&v1.Binding{
-				AppId:    "9be15160-4845-4f05-b089-40e827ba61f1",
-				Hostname: "org.space.original",
-				Drain:    "syslog://14.15.16.22/?drain-version=2.0",
-			}))
-			createReq = <-testAdapterServer.ActualCreateBindingRequest
-			Expect(createReq.Binding).To(Equal(&v1.Binding{
-				AppId:    "9be15160-4845-4f05-b089-40e827ba61f1",
-				Hostname: "org.space.new",
-				Drain:    "syslog://14.15.16.22/?drain-version=2.0",
-			}))
-		})
+		createReq = <-testAdapterServer.ActualCreateBindingRequest
+		Expect(createReq.Binding).To(Equal(&v1.Binding{
+			AppId:    "9be15160-4845-4f05-b089-40e827ba61f1",
+			Hostname: "org.space.new",
+			Drain:    "syslog://14.15.16.22/",
+		}))
 	})
 })
 
-type fakeCCWithRenamedApps struct {
-	called bool
-}
+type results map[string]appBindings
 
-func (f *fakeCCWithRenamedApps) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/internal/v4/syslog_drain_urls" {
-		w.WriteHeader(500)
-		return
-	}
-	if f.called {
-		w.Write([]byte(`
-		{
-		  "results": {
-			"9be15160-4845-4f05-b089-40e827ba61f1": {
-			  "drains": [
-                "syslog://14.15.16.22/?drain-version=2.0"
-			  ],
-			  "hostname": "org.space.new"
-			}
-		  }
-		}
-		`))
-		return
-	}
-
-	w.Write([]byte(`
-		{
-		  "results": {
-			"9be15160-4845-4f05-b089-40e827ba61f1": {
-			  "drains": [
-                "syslog://14.15.16.22/?drain-version=2.0"
-			  ],
-			  "hostname": "org.space.original"
-			}
-		  }
-		}
-	`))
-	f.called = true
+type appBindings struct {
+	Drains   []string `json:"drains"`
+	Hostname string   `json:"hostname"`
 }
 
 type fakeCC struct {
 	count           int
+	called          bool
 	withEmptyResult bool
+	withRenamedApps bool
+	results         results
 }
 
 func (f *fakeCC) ServeHTTP(w http.ResponseWriter, r *http.Request) {
@@ -325,38 +230,110 @@ func (f *fakeCC) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(500)
 		return
 	}
-	if f.count > 0 {
-		w.Write([]byte(`
-			{
-			  "results": {}
-			}
-		`))
+
+	if f.withRenamedApps {
+		f.serveWithRenamedApps(w, r)
 		return
 	}
+
+	f.serveWithResults(w, r)
+}
+
+func (f *fakeCC) serveWithRenamedApps(w http.ResponseWriter, r *http.Request) {
+	if !f.called {
+		w.Write([]byte(`
+			{
+				"results": {
+					"9be15160-4845-4f05-b089-40e827ba61f1": {
+						"drains": [
+							"syslog://14.15.16.22/"
+						],
+						"hostname": "org.space.original"
+					}
+				}
+			}
+		`))
+		f.called = true
+		return
+	}
+
 	w.Write([]byte(`
 		{
-		  "results": {
-			"9be15160-4845-4f05-b089-40e827ba61f1": {
-			  "drains": [
-                "syslog://14.15.16.21/",
-                "syslog://14.15.16.21/?drain-version=2.0",
-                "syslog://14.15.16.16/?drain-version=2.0",
-                "syslog://14.15.16.18/?drain-version=2.0",
-                "syslog://14.15.16.19/?drain-version=2.0",
-                "syslog://localhost"
-			  ],
-			  "hostname": "org.space.logspinner"
-			},
-			"ed150c22-f866-11e6-bc64-92361f002671": {
-			  "drains": [
-				"syslog://localhost"
-			  ],
-			  "hostname": "org.space.logspinner"
+			"results": {
+				"9be15160-4845-4f05-b089-40e827ba61f1": {
+					"drains": [
+						"syslog://14.15.16.22/"
+					],
+					"hostname": "org.space.new"
+				}
 			}
-		  }
 		}
 	`))
+}
+
+func (f *fakeCC) serveWithResults(w http.ResponseWriter, r *http.Request) {
+	resultData, err := json.Marshal(struct {
+		Results results `json:"results"`
+	}{
+		Results: f.results,
+	})
+	if err != nil {
+		w.WriteHeader(500)
+		return
+	}
+
+	if f.count > 0 {
+		resultData = []byte(`{"results": {}}`)
+	}
+
+	w.Write(resultData)
 	if f.withEmptyResult {
 		f.count++
 	}
+}
+
+func defaultOps() []app.SchedulerOption {
+	return []app.SchedulerOption{
+		app.WithHealthAddr("localhost:0"),
+		app.WithPollingInterval(time.Millisecond),
+	}
+}
+
+func startScheduler(dataSourceURL string, opts []app.SchedulerOption) (string, *testAdapterServer) {
+	lis, err := net.Listen("tcp", "localhost:0")
+	Expect(err).ToNot(HaveOccurred())
+
+	adapterTLSConfig, err := api.NewMutualTLSConfig(
+		Cert("adapter.crt"),
+		Cert("adapter.key"),
+		Cert("scalable-syslog-ca.crt"),
+		"adapter",
+	)
+	if err != nil {
+		log.Fatalf("Invalid TLS config: %s", err)
+	}
+
+	testAdapterServer := NewTestAdapterServer()
+	grpcServer := grpc.NewServer(
+		grpc.Creds(credentials.NewTLS(adapterTLSConfig)),
+	)
+	v1.RegisterAdapterServer(grpcServer, testAdapterServer)
+
+	go grpcServer.Serve(lis)
+
+	tlsConfig, err := api.NewMutualTLSConfig(
+		Cert("scheduler.crt"),
+		Cert("scheduler.key"),
+		Cert("scalable-syslog-ca.crt"),
+		"adapter",
+	)
+	Expect(err).ToNot(HaveOccurred())
+
+	scheduler := app.NewScheduler(
+		dataSourceURL,
+		[]string{lis.Addr().String()},
+		tlsConfig,
+		opts...,
+	)
+	return scheduler.Start(), testAdapterServer
 }
