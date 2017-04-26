aea1ed6175542371bfc653ae54c02a40f734be74
Warren Fernandes <warren.f.fernandes@gmail.com>
2017-04-26 14:30:25 -0600

    add CF_APP_DOMAIN
    Signed-off-by: JT Archie <jtarchie@gmail.com>

--- tools/smokes/push.sh
+++ tools/smokes/push.sh
@@ -8,9 +8,9 @@ pushd ./${DRAIN_TYPE}_drain
     app_name=${DRAIN_TYPE}-drain
     cf push $app_name -c ./${DRAIN_TYPE}_drain -b binary_buildpack --no-route
     if [ "$DRAIN_TYPE" == "syslog" ]; then
-        cf map-route $app_name tcp.$CF_SYSTEM_DOMAIN --random-port
+        cf map-route $app_name $CF_APP_DOMAIN --random-port
     else
-        cf map-route $app_name $CF_SYSTEM_DOMAIN --hostname $app_name
+        cf map-route $app_name $CF_APP_DOMAIN --hostname $app_name
     fi
     drain_domain=$(cf app $app_name | grep urls | awk '{print $2}')
     cf create-user-provided-service ss-smoke-syslog-${DRAIN_TYPE}-drain-${DRAIN_VERSION} -l "${DRAIN_TYPE}://$drain_domain/drain?drain-version=$DRAIN_VERSION" || true
