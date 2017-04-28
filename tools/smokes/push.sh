405c2034ebc19d36889d32471cfb786da9842503
Jason Keene <jasonkeene@gmail.com>
2017-04-28 07:09:58 -0600

    Fix pushing of smokes
    [#144236007]

--- tools/smokes/push.sh
+++ tools/smokes/push.sh
@@ -11,32 +11,48 @@ cf login \
 
 # default job_name to $DRAIN_TYPE-drain
 job_name="${JOB_NAME:-$DRAIN_TYPE-drain}"
-if cf app "$job_name"; then
-    exit 0
-fi
 
-# push the drain app (reader)
-pushd "./${DRAIN_TYPE}_drain"
-    GOOS=linux go build
-    cf push "$job_name" -c "./${DRAIN_TYPE}_drain" -b binary_buildpack --no-route
-    if [ "$DRAIN_TYPE" = "syslog" ]; then
-        cf map-route "$job_name" "$CF_APP_DOMAIN" --random-port
-    else
-        cf map-route "$job_name" "$CF_APP_DOMAIN" --hostname "$job_name"
+ensure_drain_app
+ensure_spinner_apps
+
+function ensure_drain_app {
+    if ! cf app "$job_name"; then
+        push_drain_app
     fi
-    drain_domain=$(cf app "$job_name" | grep urls | awk '{print $2}')
-    cf create-user-provided-service \
-        "ss-smoke-syslog-$job_name-drain-$DRAIN_VERSION" \
-        -l "$DRAIN_TYPE://$drain_domain/drain?drain-version=$DRAIN_VERSION" || true
-popd
+}
 
-# push logspinner apps (writers)
-pushd ../logspinner
-    GOOS=linux go build
+function ensure_spinner_apps {
     for i in $(seq 1 "$NUM_APPS"); do
-        cf push "drainspinner-$job_name-$i" -c ./logspinner -b binary_buildpack
+        if ! cf app "drainspinner-$job_name-$i"; then
+            push_spinner_app "$i"
+        fi
+    done
+}
+
+function push_drain_app {
+    pushd "./${DRAIN_TYPE}_drain"
+        GOOS=linux go build
+        cf push "$job_name" -c "./${DRAIN_TYPE}_drain" -b binary_buildpack --no-route
+        if [ "$DRAIN_TYPE" = "syslog" ]; then
+            cf map-route "$job_name" "$CF_APP_DOMAIN" --random-port
+        else
+            cf map-route "$job_name" "$CF_APP_DOMAIN" --hostname "$job_name"
+        fi
+        drain_domain=$(cf app "$job_name" | grep urls | awk '{print $2}')
+        cf create-user-provided-service \
+            "ss-smoke-syslog-$job_name-drain-$DRAIN_VERSION" \
+            -l "$DRAIN_TYPE://$drain_domain/drain?drain-version=$DRAIN_VERSION" || true
+    popd
+}
+
+function push_spinner_app {
+    pushd ../logspinner
+        if ! [ -e ./logspinner ]; then
+            GOOS=linux go build
+        fi
+        cf push "drainspinner-$job_name-$0" -c ./logspinner -b binary_buildpack
         cf bind-service \
             "drainspinner-$job_name-$i" \
             "ss-smoke-syslog-$job_name-drain-$DRAIN_VERSION"
-    done
-popd
+    popd
+}
