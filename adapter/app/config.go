771dacc05404e3bf9348339488eea6db19bf9c0c
Brady Love <blove@pivotal.io>
2017-11-02 08:53:07 -0600

    Remove required envstruct tag from fields with defaults

--- adapter/app/config.go
+++ adapter/app/config.go
@@ -9,11 +9,9 @@ import (
 	"golang.org/x/net/idna"
 )
 
+// Config stores configuration settings for the adapter.
 type Config struct {
 	SourceIndex          string        `env:"ADAPTER_INSTANCE_INDEX,  required"`
-	HealthHostport       string        `env:"HEALTH_HOSTPORT,         required"`
-	AdapterHostport      string        `env:"HOSTPORT,                required"`
-	PprofHostport        string        `env:"PPROF_HOSTPORT,          required"`
 	CAFile               string        `env:"CA_FILE_PATH,            required"`
 	CertFile             string        `env:"CERT_FILE_PATH,          required"`
 	KeyFile              string        `env:"KEY_FILE_PATH,           required"`
@@ -24,15 +22,21 @@ type Config struct {
 	RLPCommonName        string        `env:"LOGS_API_COMMON_NAME,    required"`
 	LogsAPIAddr          string        `env:"LOGS_API_ADDR,           required"`
 	LogsAPIAddrWithAZ    string        `env:"LOGS_API_ADDR_WITH_AZ,   required"`
-	SyslogDialTimeout    time.Duration `env:"SYSLOG_DIAL_TIMEOUT,     required"`
-	SyslogIOTimeout      time.Duration `env:"SYSLOG_IO_TIMEOUT,       required"`
-	SyslogSkipCertVerify bool          `env:"SYSLOG_SKIP_CERT_VERIFY, required"`
+	HealthHostport       string        `env:"HEALTH_HOSTPORT"`
+	AdapterHostport      string        `env:"HOSTPORT"`
+	PprofHostport        string        `env:"PPROF_HOSTPORT"`
+	SyslogDialTimeout    time.Duration `env:"SYSLOG_DIAL_TIMEOUT"`
+	SyslogIOTimeout      time.Duration `env:"SYSLOG_IO_TIMEOUT"`
+	SyslogSkipCertVerify bool          `env:"SYSLOG_SKIP_CERT_VERIFY"`
 
 	MetricIngressAddr     string        `env:"METRIC_INGRESS_ADDR,     required"`
 	MetricIngressCN       string        `env:"METRIC_INGRESS_CN,       required"`
-	MetricEmitterInterval time.Duration `env:"METRIC_EMITTER_INTERVAL, required"`
+	MetricEmitterInterval time.Duration `env:"METRIC_EMITTER_INTERVAL"`
 }
 
+// LoadConfig will load and validate the config from the current environment.
+// If validation fails LoadConfig will log the error and exit the process with
+// status code 1.
 func LoadConfig() *Config {
 	cfg := Config{
 		HealthHostport:        ":8080",
